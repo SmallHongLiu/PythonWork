32c9bbe89cf4fe685e65de62d8d0279e68eef3e1
SmallHong <h_q_liu@163.com>
2018-09-08 15:58:02 +0800

    panads concat合并

--- pandas_work.py
+++ pandas_work.py
@@ -113,9 +113,50 @@ print(df.isnull)
 print(np.any(df.isnull()) == True)
 '''
 
+'''
 # csv 还可以读取txt的，注意：panads读取时会自动添加索引
 data = pd.read_csv('/users/small_hong/ExcelProjects/python/student.csv')
 print(data)
 # 导出数据
 data.to_pickle('/users/small_hong/ExcelProjects/python/student.pickle')
+'''
 
+'''
+# concatenating
+df1 = pd.DataFrame(np.ones((3, 4)) * 0, columns=['a', 'b', 'c', 'd'])
+df2 = pd.DataFrame(np.ones((3, 4)) * 1, columns=['a', 'b', 'c', 'd'])
+df3 = pd.DataFrame(np.ones((3, 4)) * 2, columns=['a', 'b', 'c', 'd'])
+print(df1)
+print(df2)
+print(df3)
+# 0：竖向合并, 1: 横向合并, ignore_index：忽略index重新排序
+res = pd.concat([df1, df2, df3], axis=0, ignore_index=True)
+print(res)
+'''
+
+'''
+# join, ['inner', 'outer']
+df1 = pd.DataFrame(np.ones((3, 4)) * 0, columns=['a', 'b', 'c', 'd'], index=[1, 2, 3])
+df2 = pd.DataFrame(np.ones((3, 4)) * 1, columns=['b', 'c', 'd', 'e'], index=[2, 3, 4])
+print(df1)
+print(df2)
+# 这样合并，如果行列不相同时，会自动以Nan填充
+# res = pd.concat([df1, df2])
+# join默认值为outter，即填充合并，而inner表示裁剪合并
+# res = pd.concat([df1, df2], join='inner', ignore_index=True)
+# print(res)
+
+res = pd.concat([df1, df2], axis=1, join_axes=[df1.index])
+print(res)
+'''
+
+df1 = pd.DataFrame(np.ones((3, 4)) * 0, columns=['a', 'b', 'c', 'd'])
+df2 = pd.DataFrame(np.ones((3, 4)) * 1, columns=['a', 'b', 'c', 'd'])
+df3 = pd.DataFrame(np.ones((3, 4)) * 1, columns=['b', 'c', 'd', 'e'], index=[2, 3, 4])
+
+# 将某个数组加到某个数组上，将df2加上df1中
+# res = df1.append(df2, ignore_index=True)
+# res = df1.append([df2, df3])
+s1 = pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])
+res = df1.append(s1, ignore_index=True)
+print(res)
